95edf77b3320fc7a110589e25ed117b5c68c9dee
Rifa'i Aryo Ananta <m2992729@bangkit.academy>
2026-05-13 11:41:32 +0700

    chore: init E2E debug instrumentation on app start

--- ghenapp-web/src/main.tsx
+++ ghenapp-web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './lib/debugE2E'  // attaches window.__ghenDebug in dev
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
